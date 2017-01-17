58df3ef615308903b53c1892073dc68183eb7822
Steven Williams <dr.williams.steven@gmail.com>
2017-01-17 21:34:39 +0000

    Try to add in mongodb unsuccessfully

--- server.js
+++ server.js
@@ -2,12 +2,46 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var _ = require("underscore");
 
+// for mongoDB
+var mongo = require('mongodb').MongoClient;
+//var url = 'mongodb://localhost:27017/learnyoumongo';
+var ip_mongo = process.env.IP;
+
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
+
+// for mongoDB testing
+var firstNameVal = process.argv[2];
+var lastNameVal = process.argv[3];
+var doc = {
+      firstName: firstNameVal,
+      lastName: lastNameVal
+    };
+    
+mongo.connect(ip_mongo, function(err, db) {
+      // db gives access to the database
+      if(err){
+        console.log('There was an error: ' + err);
+      } else {
+      
+    var collection = db.collection('docs');
+      
+    collection.insert(doc, function(err, data) {
+      if(err){
+        console.log('There was an error: ' + err);
+      } else {
+        console.log(JSON.stringify(doc));
+      }
+      db.close();
+    });
+  } 
+});
+// end mongo testing
 
 app.use(express.static(__dirname + '/View'));
 //Store all HTML files in view folder.
@@ -20,10 +54,12 @@ app.get('/',function(request,response){
 });
 
 app.get('/todos', function(req, res) {
+  console.log("In comes a " + request.method + " to " + request.url);
     res.json(todos);
 });
 
 app.get('/todos/:id', function(req, res) {
+  console.log("In comes a " + request.method + " to " + request.url);
   var todoID = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoID});
   
@@ -35,6 +71,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
+  console.log("In comes a " + request.method + " to " + request.url);
   var body = _.pick(req.body, 'description', 'completed');
   
   if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
