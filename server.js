0c41b496f02c6b7aa2f2a375daacd3fb28526429
Steven Williams <dr.williams.steven@gmail.com>
2017-01-17 18:50:24 +0000

    Add underscore to POST

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require("express");
-var http = require("http");
-var url = require("url");
-var path = require("path");
+var bodyParser = require('body-parser');
+var _ = require("underscore");
 
 var app = express();
-var port = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
+var todos = [];
+var todoNextId = 1;
 
-var result;
+app.use(bodyParser.json());
 
-
+app.get('/', function(req, res) {
+    res.send('Todo API Root');
+});
 
 app.get('/todos', function(req, res) {
-  console.log("In comes a " + req.method + " to " + req.url);
-  
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
   var todoID = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoID});
   
@@ -24,12 +29,13 @@ app.get('/todos', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
   
-  if(!_.isBoolean(body.completed) || !_.isString(body.description) || ) {
+  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
     return res.status(400).send();
-    
   }
+  
+   body.description = body.description.trim();
    body.id = todoNextId++;
    todos.push(body);
    res.json(body);
@@ -37,16 +43,4 @@ app.post('/todos', function(req, res) {
 
 
 
-// Send "timestamp result"
-app.use(function(request, response) {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(result));
-});
-
-
-
-
-
-
-
- app.listen(port);
+ app.listen(PORT);
